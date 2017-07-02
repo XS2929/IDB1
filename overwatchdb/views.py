63868446ab0535be8c58fe49818da27e3a627bd3
Austin Siu <siuaustin13@gmail.com>
2017-07-01 23:44:01 -0500

    Added calls to retrieve data and pass to templates. Changed html refs.

--- overwatchdb/views.py
+++ overwatchdb/views.py
@@ -14,51 +14,57 @@ def index():
     """ Returns Welcome Page """
     return render_template('index.html')
 
-# Unsure if necessary/desirable - Austin
-# @views.route('/players/')
-# def players():
-#     """ Returns Players Page """
-#     return render_template('Players.html')
-
 
 @views.route('api/players/<player_id>', methods=['GET'])
 def get_player(player_id):
     """ Returns Page for a single Player """
-    return render_template('player.html', id=player_id)
+    data = models.Player.query.get(player_id)
+    if not data:
+        return render_template('404.html', thing='Player')
+    return render_template('player.html', data=data)
 
 
 @views.route('/api/heroes', methods=['GET'])
 def get_heroes():
     """ Returns Heroes Page """
-    return render_template('Heroes.html')
+    return render_template('heroes.html')
 
 
 @views.route('/api/heroes/<int:hero_id>', methods=['GET'])
 def get_hero(hero_id):
     """ Returns Page for a single Hero """
-    return render_template('HeroHtmls/'+hero_name+'.html', name=hero_name)
+    data = models.Hero.query.get(hero_id)
+        if not data:
+        	return render_template('404.html', thing='Hero')
+    return render_template('heroes_instance.html', data=data)
 
 
 @views.route('/api/rewards', methods=['GET'])
 def get_rewards():
     """ Returns Rewards Page """
-    return render_template('Rewards.html')
+    return render_template('rewards.html')
 
 
 @views.route('/api/rewards/<int:reward_id>', methods=['GET'])
 def get_reward(reward_id):
     """ Returns Page for a single Reward """
-    return render_template('reward.html', id=reward_id)
+    data = models.Reward.query.get(reward_id)
+    if not data:
+        return render_template('404.html', thing='Reward')
+    return render_template('rewards_Instance.html', data=data)
 
 
 @views.route('/api/achievements', methods=['GET'])
 def get_achievements():
     """ Returns Achievements Page """
-    return render_template('/Achievements.html') # id=achievement_id)
+    return render_template('/achievements.html') # id=achievement_id)
 
 
 @views.route('/api/achievements/<int:achievement_id>', methods=['GET'])
 def get_achievement(achievement_id):
+	data = models.Achievement.query.get(achievement_id)
+	if not data:
+        return render_template('404.html', thing='achievement')
     return render_template('/achievements.html', id=achievement_id)
 
 
