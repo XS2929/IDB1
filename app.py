4a0bdf44ef6e3ddb33c4493661ff8818f598837c
Austin Siu <siuaustin13@gmail.com>
2017-07-01 23:44:47 -0500

    Removed param to create_app()

---
+++ app.py
@@ -0,0 +1,9 @@
+""" Launch the application """
+import os
+from overwatchdb import create_app
+
+# pylint:disable=invalid-name
+application = create_app()
+
+if __name__ == "__main__":
+    application.run()
