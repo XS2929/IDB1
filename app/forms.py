2ca49601cd8f8730cbed45dca8fc4cebc19bca78
Austin Siu <siuaustin13@gmail.com>
2017-07-14 12:42:39 -0500

    Removed deprecation warning of FlaskForm

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,20 @@
-from flask_wtf import Form 
+from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
+class SignupForm(FlaskForm):
   first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
   last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
   email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
   password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
   submit = SubmitField('Sign up')
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired("Enter email address"), Email("Enter email address")])
   password = PasswordField('Password', validators=[DataRequired("Enter Password")])
   submit = SubmitField("Sign in")
 
-class HeroForm(Form):
+class HeroForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired("Please input hero name.")])
   age = StringField('Age', validators=[DataRequired("Please input hero age.")])
   url = StringField('url', validators=[DataRequired("Please input hero image url.")])
@@ -22,28 +22,28 @@ class HeroForm(Form):
   description = StringField('description', validators=[DataRequired("Please input hero description.")])
   submit = SubmitField("Create")
 
-class PlayerForm(Form):
+class PlayerForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired("Please input player name.")])
   level = StringField('Level', validators=[DataRequired("Please input player level.")])
   url = StringField('url', validators=[DataRequired("Please input player image url.")])
   server = StringField('server', validators=[DataRequired("Please input player server.")])
   submit = SubmitField("Create")
 
-class AchievementForm(Form):
+class AchievementForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired("Please input achievement name.")])
   description = StringField('description', validators=[DataRequired("Please input achievement description.")])
   type = StringField('type', validators=[DataRequired("Please input achievement type")])
   url = StringField('url', validators=[DataRequired("Please input achievement image url.")])
   submit = SubmitField("Create")
 
-class RewardForm(Form):
+class RewardForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired("Please input reward name.")])
   quality = StringField('quality', validators=[DataRequired("Please input reward quality.")])
   cost = IntegerField('cost', validators=[DataRequired("Please input achievement cost")])
   url = StringField('url', validators=[DataRequired("Please input reward image url.")])
   submit = SubmitField("Create")
 
-class DeleteForm(Form):
+class DeleteForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired("Please input name of entity to delete.")])
   model = StringField('Model', validators=[DataRequired("Please input model/category of entity to delete i.e. Hero, Player, Achievement, Reward.")])
   submit = SubmitField("Delete")
